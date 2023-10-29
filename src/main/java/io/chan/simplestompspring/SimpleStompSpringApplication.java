aa0ce1c95b071dcc00dea16131a58f0cfe873d22
Chan <33cks1423@naver.com>
2023-10-29 19:43:46 +0900

    feat: project init

---
+++ src/main/java/io/chan/simplestompspring/SimpleStompSpringApplication.java
@@ -0,0 +1,13 @@
+package io.chan.simplestompspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleStompSpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SimpleStompSpringApplication.class, args);
+    }
+
+}
